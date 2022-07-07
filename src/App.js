851d64a4434ec785fc86eff16054456092e72c5c
Max <main@Maxs-MacBook.lan>
2022-07-07 14:21:27 -0500

    Clone repo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+import React from "react";
+
+export default function App() {
+  const [count, setCount] = React.useState(-1);
+  const isStart = count < 0;
+  const onClick = React.useCallback(() => {
+    setCount(isStart ? 3 : -1);
+  }, [isStart]);
+
+  React.useEffect(() => {
+    if (count < 0) {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => setCount(count - 1), 1000);
+    return () => clearTimeout(timeoutId);
+  }, [count]);
+
+  return (
+    <div className="App">
+      <button onClick={onClick}>
+        {isStart ? "Start Countdown" : "Cancel"}
+      </button>
+      {!isStart && <h1>{count > 0 ? count : "GO"}</h1>}
+    </div>
+  );
+}
